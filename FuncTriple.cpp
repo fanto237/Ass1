3ae5e3f88fdb0b1a5412e639b31e280dfacf9e64
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-11-23 13:58:35 +0100

    update compare operator

--- FuncTriple.cpp
+++ FuncTriple.cpp
@@ -6,7 +6,6 @@ FuncTriple::FuncTriple(const Func &iVar, const Func &fThen, const Func &fElse) :
 
 bool operator<(const FuncTriple &crArg1, const FuncTriple &crArg2) {
     return &crArg1.m_ciVar < &crArg2.m_ciVar ||
-           (&crArg1.m_ciVar == &crArg2.m_ciVar && &crArg1.m_cThen < &crArg2.m_cThen)
-           || (&crArg1.m_ciVar == &crArg2.m_ciVar && &crArg1.m_cThen == &crArg2.m_cThen
-               && &crArg1.m_cElse < &crArg2.m_cElse);
+           (&crArg1.m_ciVar == &crArg2.m_ciVar && &crArg1.m_cThen < &crArg2.m_cThen) ||
+           (&crArg1.m_ciVar == &crArg2.m_ciVar && &crArg1.m_cThen == &crArg2.m_cThen && &crArg1.m_cElse < &crArg2.m_cElse);
 }
